21df0babd8066011b4264faa2e4b75476c169824
Nicki <alternativelenicki@live.dk>
2020-09-01 11:18:25 +0200

    Lavet ændringer til PieReadClient og Server for at få Temperatur og Humidity ind i brugbare variabler

--- src/PieReadTest/PieReadClient.java
+++ src/PieReadTest/PieReadClient.java
@@ -1,20 +1,20 @@
 package PieReadTest;
 
-import javafx.*;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
-public class PieReadClient {
+public class PieReadClient{
 
     static int port = 420;
     static String host = "192.168.50.89";
     static DataInputStream in;
     static DataOutputStream out;
     static Socket socket;
+
 
     public static void main(String[] args) {
         try{
@@ -23,16 +23,18 @@ public class PieReadClient {
             socket = new Socket(host,port);
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
-            double temp, humid;
             boolean serverMsg = true;
 //            while (serverMsg = true) {
                 byte[] message = in.readAllBytes();
-                String str = new String(message, StandardCharsets.UTF_8);
-                temp = in.readDouble();
-                humid = in.readDouble();
-                System.out.println(str + ' ' + temp + ' ' + humid);
+                String strMessage = new String(message, StandardCharsets.UTF_8);
+                byte[] byteTemp = in.readAllBytes();
+                String temp = new String(byteTemp,StandardCharsets.UTF_8);
+                byte[] byteHumid = in.readAllBytes();
+                String humid = new String(byteHumid, StandardCharsets.UTF_8);
+                System.out.println(strMessage + ' ' + temp + ' ' + humid);
                 Thread.sleep(5000);
                 socket.close();
+
 
             }
         } catch (IOException | InterruptedException e) {
